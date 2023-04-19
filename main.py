d16178f3a0c5e9d4b431f4333018d44d3d935a30
Kennedy <kennedybacelar@yahoo.com.br>
2023-04-19 01:33:29 -0300

    changing typing

--- main.py
+++ main.py
@@ -8,7 +8,17 @@ import os
 class DataFile:
     def __init__(self, file_path: Path):
         self.file_path = file_path
-        self.dataframe = pd.read_excel(file_path)
+        self.dataframe = pd.read_excel(file_path, sheet_name='input_refresh_template')
+        self.validate_data_frame()
+        
+    def validate_data_frame(self):
+        start_and_end_dates = self.dataframe["A"].head(2)
+        try:
+            start_and_end_dates_as_datetime = pd.to_datetime(start_and_end_dates).tolist()
+            self.start_date = start_and_end_dates_as_datetime[0]
+            self.end_date = start_and_end_dates_as_datetime[1]
+        except ValueError as error:
+            raise ValueError(f"Not able to parse dates correctly from the excel template {error}")
 
 
 def get_input_file_paths() -> List[Path]:
