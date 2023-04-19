2e0a911f14b198a09c3a4742a9e701dfb2d1ff34
Kennedy <kennedybacelar@yahoo.com.br>
2023-04-19 01:43:41 -0300

    adding validate_data_frame

--- main.py
+++ main.py
@@ -8,15 +8,14 @@ import os
 class DataFile:
     def __init__(self, file_path: Path):
         self.file_path = file_path
-        self.dataframe = pd.read_excel(file_path, sheet_name='input_refresh_template')
+        self.dataframe = pd.read_excel(file_path, sheet_name='input_refresh_template', header=None)
         self.validate_data_frame()
         
     def validate_data_frame(self):
-        start_and_end_dates = self.dataframe["A"].head(2)
+        start_and_end_dates = self.dataframe.iloc[0:2, 0]
         try:
             start_and_end_dates_as_datetime = pd.to_datetime(start_and_end_dates).tolist()
-            self.start_date = start_and_end_dates_as_datetime[0]
-            self.end_date = start_and_end_dates_as_datetime[1]
+            self.start_date, self.end_date = [date.to_pydatetime() for date in start_and_end_dates_as_datetime]
         except ValueError as error:
             raise ValueError(f"Not able to parse dates correctly from the excel template {error}")
 
@@ -24,4 +23,3 @@ class DataFile:
 def get_input_file_paths() -> List[Path]:
     input_path_str = load_config()["input_path"]
     return [Path(os.path.join(input_path_str, file_path_str)) for file_path_str in os.listdir(input_path_str)]
-
